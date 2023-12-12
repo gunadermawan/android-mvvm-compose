b952dd7608c24ea00189c0631fb75acb397bdc02
gunadermawan <gunadermawan.official@gmail.com>
2023-12-12 10:08:09 +0700

    add hilt dependency and config it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -69,6 +73,12 @@ dependencies {
     //Coroutine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
     // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
+kapt {
+    correctErrorTypes = true
+}
+
---
+++ app/src/main/java/com/gunder/mvvm/data/remote/ApiModule.kt
@@ -0,0 +1,7 @@
+package com.gunder.mvvm.data.remote
+
+import dagger.Module
+
+@Module
+object ApiModule {
+}
